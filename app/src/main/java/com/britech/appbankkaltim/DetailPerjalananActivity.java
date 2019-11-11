4a858dfb34539dcae7af7100b09d60a0ec03dea6
DESKTOP-164PVQ6\ilham ilyas <ilhamilyas100@gmail.com>
2019-11-12 00:30:23 +0800

    layout done

---
+++ app/src/main/java/com/britech/appbankkaltim/DetailPerjalananActivity.java
@@ -0,0 +1,14 @@
+package com.britech.appbankkaltim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailPerjalananActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_perjalanan);
+    }
+}
---
+++ app/src/main/java/com/britech/appbankkaltim/UploadFotoActivity.java
@@ -0,0 +1,14 @@
+package com.britech.appbankkaltim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadFotoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_foto);
+    }
+}
